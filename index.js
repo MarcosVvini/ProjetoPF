f668944b3352432cda1a018916094a3288b2cfaa
MarcosVvini <164093887+MarcosVvini@users.noreply.github.com>
2024-04-08 21:44:25 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Funcoes relacionadas a leitura e manipulação de arquivos CSV
 const lerArquivoCsv = async (caminhoArquivo) => {
     try {
-        const response = await fetch(caminhoArquivo);
-        if (!response.ok) {
+        const response = await fetch(caminhoArquivo); //faz uma solicitação para pegar o caminho do arquivo
+        if (!response.ok) { //verifica se a solicitação deu certo.
             throw new Error('Falha ao carregar o arquivo');
         }
         const csvContent = await response.text();
@@ -11,50 +11,65 @@ const lerArquivoCsv = async (caminhoArquivo) => {
         throw err;
     }
 };
-
 const parseCSV = (csv) => {
-    const lines = csv.split(/\r?\n/);
-    const [cabecalho, ...linhasDados] = lines;
+    const lines = csv.split(/\r?\n/); //divide em linhas
+    const [cabecalho, ...linhasDados] = lines; //separa o cabeçalho e as linhas
 
-    const camposCabecalho = cabecalho.split(',');
-    const camposCabecalhoFormatados = camposCabecalho.map(item => item.replace(/"/g, ''));
+    const formatarCampo = (campo) => campo.replace(/"/g, ''); //função que remove as aspas
 
-    const dados = linhasDados.map(linha => {
-        const campos = linha.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-        const objeto = {};
-        campos.forEach((campo, index) => {
-            const campoFormatado = campo.replace(/"/g, '');
-            objeto[camposCabecalhoFormatados[index]] = campoFormatado;
-        });
-        return objeto;
-    });
+    const camposCabecalhoFormatados = cabecalho.split(',').map(formatarCampo);
 
+    //função para criar uma lista de registros
+    const parseLinha = (linha) => {
+        const campos = linha.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);//divide os campos, mesmo que tenha "," nas strings, não irá afetar o resultado
+        return campos.reduce((objeto, campo, index) => {
+            const campoFormatado = formatarCampo(campo);
+            return { ...objeto, [camposCabecalhoFormatados[index]]: campoFormatado };
+        }, {});
+    };
+    //aplicação da função parseLinha nos dados
+    const dados = linhasDados.map(parseLinha);
+
+    //como retorno uma lista de registros com as seguintes chaves e os seus respectivos valores
     return dados.map(item => ({
         ID: item.ID,
         Name: item.Name,
         Age: item.Age,
-        Team: item.Team ? item.Team.replace('-1', '') : '', //testando pois tive problema com undefined. to trocando o -1 por str vazia pois alguns times eram Brazil-1 por ex...
+        Team: item.Team ? item.Team.replace('-1', '') : '', //testando pois tive problema com undefined. to trocando o -1 por str vazia pois alguns Team eram Brazil-1 por ex...
         Year: parseInt(item.Year),
         Season: item.Season,
         Sport: item.Sport,
         Medal: item.Medal
     }));
 };
-
 // Constante para o caminho do arquivo de atletas
 const caminhoArquivoAtletas = 'athlete_events.csv';
 
 // Funcoes relacionadas ao filtro e processamento dos dados dos atletas
+/* 
+essa função é resoponsavel por filtrar os atletas de acordo com a nacionalidade e ano, recebendo como parametro
+a lista (que serão os dados do csv processado), o pais (fornecido pelo usuario) e o ano (tambem fornecido pelo usuario)
+e retorna uma lista com os atletas filtrados de acordo com os parametros passados pelo usuario!
+*/
 const atletasNacionalidadeAno = (lista) => (pais) => (ano) => {
     const atletas = lista.filter(item => item.Team === pais && item.Year === ano);
     return removerDuplicatas(atletas);
 };
-
+/* 
+essa função é resoponsavel por filtrar os atletas de acordo com a nacionalidade, medalha e ano, recebendo como parametro
+a lista (que serão os dados do csv processado), o pais (fornecido pelo usuario), a medalha (fornecida pelo usuario) e o ano (tambem fornecido pelo usuario)
+e retorna uma lista com os atletas filtrados de acordo com os parametros passados pelo usuario!
+*/
 const atletasMedalhaAno = (lista) => (pais) => (medalha) => (ano) => {
     const medalhistas = lista.filter(item => item.Team === pais && item.Medal === medalha && item.Year === ano);
     return removerDuplicatas(medalhistas);
 };
-
+/* 
+essas 2 funções são resoponsaveis respectivamente por mostrar o atleta mais velho e mais novo, 
+filtrando os atletas por nacionalidade e ano, recebendo como parametro
+a lista (que serão os dados do csv processado), o pais (fornecido pelo usuario) e o ano (tambem fornecido pelo usuario)
+e retorna o atleta mais velho e mais novo de acordo com os parametros passados pelo usuario!
+*/
 const encontrarAtletaMaisVelho = (lista) => (pais) => (ano) => {
     const atletasDoPaisNoAno = lista.filter(item => item.Team === pais && item.Year === ano);
     if (atletasDoPaisNoAno.length === 0) {
@@ -63,7 +78,6 @@ const encontrarAtletaMaisVelho = (lista) => (pais) => (ano) => {
     const atletaMaisVelho = atletasDoPaisNoAno.reduce((maisVelho, atleta) => atleta.Age > maisVelho.Age ? atleta : maisVelho);
     return atletaMaisVelho;
 };
-
 const encontrarAtletaMaisNovo = (lista) => (pais) => (ano) => {
     const atletasDoPaisNoAno = lista.filter(item => item.Team === pais && item.Year === ano);
     if (atletasDoPaisNoAno.length === 0) {
@@ -72,7 +86,11 @@ const encontrarAtletaMaisNovo = (lista) => (pais) => (ano) => {
     const atletaMaisNovo = atletasDoPaisNoAno.reduce((maisNovo, atleta) => atleta.Age < maisNovo.Age ? atleta : maisNovo);
     return atletaMaisNovo;
 };
-
+/* 
+essa função é resoponsavel por filtrar os atletas de acordo com a letra e ano, recebendo como parametro
+a lista (que serão os dados do csv processado), a letra (fornecido pelo usuario) e o ano (tambem fornecido pelo usuario)
+e retorna uma lista com os atletas Sulamericanos filtrados de acordo com os parametros passados pelo usuario!
+*/
 const AtletasqueParticiparamAno = (lista) => (letra) => (ano) => {
     const filt1 = lista.filter((p) => (p.Team == "Argentina") || (p.Team == "Bolivia") || (p.Team == "Brazil") 
         || (p.Team == "Chile") || (p.Team == "Colombia") || (p.Team == "Ecuador") || (p.Team == "Guyana") || (p.Team == "Paraguay")
@@ -81,38 +99,42 @@ const AtletasqueParticiparamAno = (lista) => (letra) => (ano) => {
     const filt3 = filt2.filter((a) => a.Year == ano)
     return removerDuplicatas(filt3)
 }
-
+/* 
+essa função é responsavel por comparar a quantidade de medalhas que os atletas de 2 determinados paises ganharam, recebendo como parametro
+a lista (que serão os dados do csv processado), os 2 paises (fornecidos pelo usuario), e o ano (tambem fornecido pelo usuario)
+e retorna uma lista com a quantidade de medalhas (ouro prata e bronze) adiquiridas pelos atletas dos paises de acordo com os parametros passados pelo usuario!
+*/
 const totalMedalhasPorPais = (lista) => (pais1) => (pais2) => (ano) => {
     const atletasDoPais1NoAno = lista.filter(item => item.Team === pais1 && item.Year === ano && item.Medal !== '');
     const atletasDoPais2NoAno = lista.filter(item => item.Team === pais2 && item.Year === ano && item.Medal !== '');
-
-
     const medalhasPorAtleta1 = atletasDoPais1NoAno.map(atleta => atleta.Medal);
     const medalhasPorAtleta2 = atletasDoPais2NoAno.map(atleta => atleta.Medal);
 
     const contagemMedalhas1 = medalhasPorAtleta1.reduce((contador, medalha) => {
         return {
-            ...contador,
-            [medalha]: (contador[medalha] || 0) + 1
+            ...contador, [medalha]: (contador[medalha] || 0) + 1 //-----------------????????
         };
     }, { Gold: 0, Silver: 0, Bronze: 0 });
+
     const contagemMedalhas2 = medalhasPorAtleta2.reduce((contador, medalha) => {
         return {
-            ...contador,
-            [medalha]: (contador[medalha] || 0) + 1
+            ...contador, [medalha]: (contador[medalha] || 0) + 1
         };
     }, { Gold: 0, Silver: 0, Bronze: 0 });
 
     return [contagemMedalhas1,contagemMedalhas2];
 };
-  
-
+/* 
+essa função é resoponsavel por filtrar os atletas que participaram mais vezes, pois em alguns casos vou querer apenas saber se ele participou ou ganhou
+então so precisarei que ele apareça uma vez na lista, vai receber uma lista ja filtrada para o resultado como parametro e comparar 
+os ids para ter apenas 1 por atleta, logo, não haverão repetições e retorna uma lista com os atletas sem repetições!
+*/
 const removerDuplicatas = (lista) => lista.reduce((acc, item) => {
     const idPresente = acc.some(existingItem => existingItem.ID == item.ID);
     return idPresente ? acc : [...acc, item];
 }, []);
 
-// Funcao relacionada a adição de opções nos elementos <select> do HTML
+// Funcao relacionada a adição de opções nos elementos <select> do HTML, que recebe o id do select e a lista com as opções
 const adicionarOpcaoSelect = (id, opcoes) => {
     const select = document.querySelector(`#${id}`);
     const optionsElements = opcoes.map(opcao => {
@@ -124,31 +146,29 @@ const adicionarOpcaoSelect = (id, opcoes) => {
     select.append(...optionsElements);
 };
 
+
+//funções que irão pegar o valor selecionado pelo usuario no select
 const valSelecQ1 = (selectPais, selectAno) => {
     const paisSelecionado = selectPais.value;
     const anoSelecionado = parseInt(selectAno.value);
     return [paisSelecionado, anoSelecionado];
 };
-
 const valSelecQ2 = (selectPaisQ2, selectMedalhaQ2, selectAnoQ2) => {
     const paisSelecionadoQ2 = selectPaisQ2.value;
     const medalhaSelecionadaQ2 = selectMedalhaQ2.value;
     const anoSelecionadoQ2 = parseInt(selectAnoQ2.value);
     return [paisSelecionadoQ2, medalhaSelecionadaQ2, anoSelecionadoQ2];
 };
-
 const valSelecQ3 = (selectLetraQ3, selectAnoQ3) => {
     const letraSelecionadaQ3 = selectLetraQ3.value;
     const anoSelecionadoQ3 = parseInt(selectAnoQ3.value);
     return [letraSelecionadaQ3, anoSelecionadoQ3];
 };
-
 const valSelecQ4 = (selectPaisQ4, selectAnoQ4) => {
     const paisSelecionadoQ4 = selectPaisQ4.value;
     const anoSelecionadoQ4 = parseInt(selectAnoQ4.value);
     return [paisSelecionadoQ4, anoSelecionadoQ4];
 };
-
 const valSelecQ5 = (selectPaisQ51,selectPaisQ52, selectAnoQ5) => {
     const paisSelecionadoQ51 = selectPaisQ51.value;
     const paisSelecionadoQ52 = selectPaisQ52.value;
@@ -158,11 +178,13 @@ const valSelecQ5 = (selectPaisQ51,selectPaisQ52, selectAnoQ5) => {
 
 // Event listeners para os botoes de busca
 document.addEventListener("DOMContentLoaded", async () => {
+    //listas dos selects!
     const paises = ["Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"];
     const anos = [2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014, 2016];
     const medalha = ["Gold", "Silver", "Bronze"]
     const letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+    //adicionando as opções no select pelo id dele, passando as listas para povoa-los.
     adicionarOpcaoSelect("listaPaisQ1", paises);
     adicionarOpcaoSelect("listaAnoQ1", anos);
     adicionarOpcaoSelect("listaPaisQ2", paises);
@@ -176,16 +198,17 @@ document.addEventListener("DOMContentLoaded", async () => {
     adicionarOpcaoSelect("listaPaisQ5-2", paises);
     adicionarOpcaoSelect("listaAnoQ5", anos);
 
+    //função para o botão buscar que vai fazer a busca dos atletas de acordo com os filtros selecionados.
     const botaoBuscar = document.querySelector('#botao-buscar-q1');
     botaoBuscar.addEventListener("click", async () => {
         const selectPais = document.querySelector("#listaPaisQ1");
         const selectAno = document.querySelector("#listaAnoQ1");
-        const [paisSelecionado, anoSelecionado] = valSelecQ1(selectPais, selectAno);
+        const [paisSelecionado, anoSelecionado] = valSelecQ1(selectPais, selectAno); //pega os valores selecionados pelo usuario
 
         try {
-            document.getElementById('spinner-q1').classList.remove('d-none');
+            document.getElementById('spinner-q1').classList.remove('d-none');//spinner aparece na tela e fica girando ao clicar no botao.
 
-            const conteudoArquivo = await lerArquivoCsv(caminhoArquivoAtletas);
+            const conteudoArquivo = await lerArquivoCsv(caminhoArquivoAtletas); //await para so fazer os proximos passos quando conseguir ler o arquivo
             const csvAtletas = parseCSV(conteudoArquivo);
 
             const filtroQ1 = atletasNacionalidadeAno(csvAtletas);
@@ -201,6 +224,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 </tr>
             `).join('');
 
+            //cria o cabeçalho da tabela resultado depois depois chama o listaAtletasHTML para povoar a tabela com os resultados
             tableBody.innerHTML = `
                 <h3>${resultadoFiltro.length} Atletas.</h3>
                 <tr>
@@ -213,7 +237,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         } catch (err) {
             console.error('Erro ao ler o arquivo:', err);
         } finally {
-            document.getElementById('spinner-q1').classList.add('d-none');
+            document.getElementById('spinner-q1').classList.add('d-none');//quando acabar de pesquisar e mostrar o resultado spinner desaparece da tela.
         }
     });
 
